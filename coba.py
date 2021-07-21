d8128f0468e16360700e535c316fa49cd22838c4
holndal <ryouta0322a@yahoo.co.jp>
2021-07-21 11:18:52 +0900

    rm detail

--- coba.py
+++ coba.py
@@ -21,9 +21,6 @@ def coba(params: List[Tensor],
          weight_decay: float,
          eps: float,
          gammatype: str):
-    r"""Functional API that performs Adam algorithm computation.
-    See :class:`~torch.optim.Adam` for details.
-    """
 
     for i, param in enumerate(params):
         grad = grads[i]
@@ -71,30 +68,6 @@ def coba(params: List[Tensor],
         param.addcdiv_(exp_avg, denom, value=-step_size)
 
 class CoBA(Optimizer):
-    r"""Implements Adam algorithm.
-    It has been proposed in `Adam: A Method for Stochastic Optimization`_.
-    The implementation of the L2 penalty follows changes proposed in
-    `Decoupled Weight Decay Regularization`_.
-    Args:
-        params (iterable): iterable of parameters to optimize or dicts defining
-            parameter groups
-        lr (float, optional): learning rate (default: 1e-3)
-        betas (Tuple[float, float], optional): coefficients used for computing
-            running averages of gradient and its square (default: (0.9, 0.999))
-        eps (float, optional): term added to the denominator to improve
-            numerical stability (default: 1e-8)
-        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
-        amsgrad (boolean, optional): whether to use the AMSGrad variant of this
-            algorithm from the paper `On the Convergence of Adam and Beyond`_
-            (default: False)
-    .. _Adam\: A Method for Stochastic Optimization:
-        https://arxiv.org/abs/1412.6980
-    .. _Decoupled Weight Decay Regularization:
-        https://arxiv.org/abs/1711.05101
-    .. _On the Convergence of Adam and Beyond:
-        https://openreview.net/forum?id=ryQu7f-RZ
-    """
-
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
                  weight_decay=0, amsgrad=False, gammatype="FR"):
         if not 0.0 <= lr:
@@ -118,12 +91,7 @@ class CoBA(Optimizer):
 
     @torch.no_grad()
     def step(self, closure=None):
-        """Performs a single optimization step.
-        Args:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
-        loss = None
+       loss = None
         if closure is not None:
             with torch.enable_grad():
                 loss = closure()
